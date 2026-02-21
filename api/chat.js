26c3a5e032de853214bce5249f5c02ade0fc280f
dhannjayuphade <dhannjayuphade5@gmail.com>
2026-02-21 13:27:42 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -29,3 +29,5 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: error.message });
   }
                                 }
+console.log("Incoming message:", message);
+console.log("API response:", data);
